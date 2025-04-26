7075c18cc103c2835c069cd69bea27eb8180841f
arbaz93 <arbaz.k480@yahoo.com>
2025-04-26 07:12:00 +0500

    fixed bug in firebase,js line:02

--- api/firebase/firebase.js
+++ api/firebase/firebase.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_KEY);
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
